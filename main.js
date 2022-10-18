d7ae723008c652b99ca8011a4f69656a219c0b50
MITANSH12 <78300422+MITANSH12@users.noreply.github.com>
2022-10-18 19:52:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function preload() {
+classifier = ml5.imageClassifier('DoodleNet');
+}
+function setup() {
+canvas = createCanvas(280 , 280);
+canvas.center();
+background("white");
+canvas.mouseReleased(classifyCanvas);
+synth = window.speechSynthesis;
+}
+function draw() {
+strokeWeight(5);
+stroke("black");
+if (mouseIsPressed) {
+    line(pmouseX , pmouseY , mouseX , mouseY);
+}
+}
+function clearCanvas() {
+    background("white");
+}
+function classifyCanvas() {
+    classifier.classify(canvas , gotResult);
+
+}
+function gotResult (error , results) {
+if (error) {
+    console.error(error);
+}
+else {
+    console.log(results);
+    document.getElementById("label").innerHTML = "Label:" + results[0].label;
+    document.getElementById("confidence").innerHTML = "Confidence:" + Math.round(results[0].confidence * 100) + "%";
+    speakData = results[0].label;
+    utterThis = new SpeechSynthesisUtterance(speakData);
+    synth.speak(utterThis);
+}
+}
